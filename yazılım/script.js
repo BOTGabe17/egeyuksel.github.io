a198548b8b7887e4eae5e6fd27d4cdda189a6261
BOTGabe17 <107195634+BOTGabe17@users.noreply.github.com>
2023-05-19 14:38:33 +0300

    Update script.js

--- yazılım/script.js
+++ yazılım/script.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     var sesliButon = document.getElementById("sesli-buton");
     var butonResim = document.getElementById("buton-resim");
-    var ses = new Audio("Turks_jingle_AoE2_DE.ogg");
+    var ses = new Audio("../galeri/Turks_jingle_AoE2_DE.ogg");
   
     sesliButon.addEventListener("click", function() {
       if (ses.paused) {
         ses.play();
         sesliButon.classList.add("playing");
-        butonResim.src = "https://github.com/BOTGabe17/egeyuksel.github.io/blob/main/galeri/dur.png";
+        butonResim.src = "../galeri/dur.png";
       } else {
         ses.pause();
         ses.currentTime = 0; // Sesin başa dönmesini sağlar
         sesliButon.classList.remove("playing");
-        butonResim.src = "https://github.com/BOTGabe17/egeyuksel.github.io/blob/main/galeri/oynat.png";
+        butonResim.src = "../galeri/oynat.png";
       }
     });
   
     ses.addEventListener("ended", function() {
       sesliButon.classList.remove("playing");
-      butonResim.src = "https://github.com/BOTGabe17/egeyuksel.github.io/blob/main/galeri/oynat.png";
+      butonResim.src = "../galeri/oynat.png";
     });
   });
   
