f1c79991d5026d8ee1daee2231deca419b0962fe
BOTGabe17 <107195634+BOTGabe17@users.noreply.github.com>
2023-05-19 03:17:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
       if (ses.paused) {
         ses.play();
         sesliButon.classList.add("playing");
-        butonResim.src = "dur.png";
+        butonResim.src = "https://raw.githubusercontent.com/BOTGabe17/egeyuksel.github.io/main/dur.png";
       } else {
         ses.pause();
         ses.currentTime = 0; // Sesin başa dönmesini sağlar
         sesliButon.classList.remove("playing");
-        butonResim.src = "oynat.png";
+        butonResim.src = "https://raw.githubusercontent.com/BOTGabe17/egeyuksel.github.io/main/oynat.png";
       }
     });
   
     ses.addEventListener("ended", function() {
       sesliButon.classList.remove("playing");
-      butonResim.src = "oynat.png";
+      butonResim.src = "https://raw.githubusercontent.com/BOTGabe17/egeyuksel.github.io/main/oynat.png";
     });
   });
